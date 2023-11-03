2d592d5d9da455ef53639fac0f9afcc9550f65ab
Muhammad Usman <muhammadusmanismail97@gmail.com>
2023-11-03 14:35:11 +0500

    test commit

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -4,7 +4,7 @@ module SpreeMultiDomain
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    def self.activate
+    config.to_prepare do
       ['app', 'lib'].each do |dir|
         Dir.glob(File.join(File.dirname(__FILE__), "../../#{dir}/**/*_decorator*.rb")) do |c|
           Rails.application.config.cache_classes ? require(c) : load(c)
@@ -15,46 +15,51 @@ module SpreeMultiDomain
       ApplicationController.send :include, SpreeMultiDomain::MultiDomainHelpers
     end
 
-    config.to_prepare &method(:activate).to_proc
+    initializer "templates with dynamic layouts" do
+      ActiveSupport.on_load(:action_view) do
+        ActionView::TemplateRenderer.class_eval do
+          def find_layout_with_multi_store(layout, locals)
+            store_layout = layout
 
-    initializer "templates with dynamic layouts" do |app|
-      ActionView::TemplateRenderer.class_eval do
-        def find_layout_with_multi_store(layout, locals)
-          store_layout = layout
+            if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController)
+              store_layout = if layout.is_a?(String)
+                               layout.gsub("layouts/", "layouts/#{@view.current_store.code}/")
+                             else
+                               layout.call.try(:gsub, "layouts/", "layouts/#{@view.current_store.code}/")
+                             end
+            end
 
-          if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController)
-            store_layout = if layout.is_a?(String)
-              layout.gsub("layouts/", "layouts/#{@view.current_store.code}/")
-            else
-              layout.call.try(:gsub, "layouts/", "layouts/#{@view.current_store.code}/")
+            begin
+              find_layout_without_multi_store(store_layout, locals)
+            rescue ::ActionView::MissingTemplate
+              find_layout_without_multi_store(layout, locals)
             end
           end
 
-          begin
-            find_layout_without_multi_store(store_layout, locals)
-          rescue ::ActionView::MissingTemplate
-            find_layout_without_multi_store(layout, locals)
-          end
+          alias_method :find_layout_without_multi_store, :find_layout
+          alias_method :find_layout, :find_layout_with_multi_store
         end
-
-        alias_method_chain :find_layout, :multi_store
       end
     end
 
-    initializer "current order decoration" do |app|
-      require 'spree/core/controller_helpers/order'
-      ::Spree::Core::ControllerHelpers::Order.module_eval do
-        def current_order_with_multi_domain(options = {})
-          options[:create_order_if_necessary] ||= false
-          current_order_without_multi_domain(options)
+    initializer "current order decoration" do
+      ActiveSupport.on_load(:action_controller) do
+        require 'spree/core/controller_helpers/order'
+        ::Spree::Core::ControllerHelpers::Order.module_eval do
+          def current_order_with_multi_domain(options = {})
+            options[:create_order_if_necessary] ||= false
+            current_order_without_multi_domain(options)
 
-          if @current_order and current_store and @current_order.store.blank?
-            @current_order.update_attribute(:store_id, current_store.id)
+            if @current_order && current_store && @current_order.store.blank?
+              @current_order.update_attribute(:store_id, current_store.id)
+            end
+
+            @current_order
           end
 
-          @current_order
+          alias_method :current_order_without_multi_domain, :current_order
+          alias_method :current_order, :current_order_with_multi_domain
         end
-        alias_method_chain :current_order, :multi_domain
       end
     end
 
